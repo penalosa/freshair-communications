7effe253f2c7f1bce7e5e625c3bacef5cd9dacd0
Samuel Macleod <penalosa@samuels-mbp.home>
2019-11-09 08:47:09 +0000

    Basic tweeting

--- workers/tweetbot.js
+++ workers/tweetbot.js
@@ -1,10 +1,24 @@
+const Twit = require("twit");
+
 module.exports = {
   domain: "freshair.org.uk",
   namespaces: ["secrets"],
   handlers: ({ json, html, text }, { secrets }) => ({
     get: {
       "/": async (req, _, log) => {
-        return text("Hello World");
+        const T = new Twit({
+          consumer_key: await secrets.get("twitter_api_key"),
+          consumer_secret: await secrets.get("twitter_api_secret_key"),
+          access_token: await secrets.get("twitter_access_token"),
+          access_token_secret: await secrets.get("twitter_access_token_secret")
+        });
+        T.post("statuses/update", { status: "hello world!" }, function(
+          err,
+          data,
+          response
+        ) {
+          console.log(data);
+        });
       }
     }
   })
