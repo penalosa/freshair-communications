872b9b3aab7656308740b6c39ee24451508c943d
Samuel Macleod <penalosa@samuels-mbp.home>
2019-11-09 09:21:09 +0000

    p

--- workers/tweetbot.js
+++ workers/tweetbot.js
@@ -4,21 +4,22 @@ module.exports = {
   domain: "freshair.org.uk",
   namespaces: ["secrets"],
   handlers: ({ json, html, text }, { secrets }) => ({
-    get: {
-      "/": async (req, _, log) => {
+    put: {
+      "/tweet": async (req, _, log) => {
+        let body = await req.json();
         const T = new Twit({
           consumer_key: await secrets.get("twitter_api_key"),
           consumer_secret: await secrets.get("twitter_api_secret_key"),
           access_token: await secrets.get("twitter_access_token"),
           access_token_secret: await secrets.get("twitter_access_token_secret")
         });
-        T.post("statuses/update", { status: "Hello World!" }, function(
-          err,
-          data,
-          response
-        ) {
-          console.log(data);
-        });
+        T.post(
+          "statuses/update",
+          { status: `Live now: ${body.show_name}` },
+          function(err, data, response) {
+            console.log(data);
+          }
+        );
       }
     }
   })
